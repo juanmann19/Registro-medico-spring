6445c79cf560cbb66309c423fc8a4c72e81be6b4
Juan Garcia <Juanmanu2350@gmail.com>
2023-06-12 13:28:49 -0600

    Persistence applied to Medicos and Direccion

---
+++ src/main/java/med/boll/api/direccion/Direccion.java
@@ -0,0 +1,20 @@
+package med.boll.api.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Direccion {
+    private String calle;
+    private  Integer numero;
+    private String complemento;
+    private String distrito;
+    private String ciudad;
+
+}
---
+++ src/main/java/med/boll/api/medico/Medico.java
@@ -0,0 +1,32 @@
+package med.boll.api.medico;
+
+import med.boll.api.direccion.Direccion;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+
+
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+
+}
